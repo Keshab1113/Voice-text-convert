36d69ab109731b1a38c8047fe443c7b3a8d47db7
Keshab <keshabdas2003@gmail.com>
2025-08-23 16:10:14 +0530

    fix

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-export const API_BASE = 'http://localhost:5000';
+// export const API_BASE = 'http://localhost:5000';
+export const API_BASE = 'https://voice-text-convert.onrender.com';
 
 export const api = axios.create({
   baseURL: `${API_BASE}/api`,
